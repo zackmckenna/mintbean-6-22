fff4ff76e94e5c90e8aecbcc56693e44d791a800
Zack McKenna <zackmckenna@gmail.com>
2020-06-24 12:01:27 -0400

    changed CI env variable to false

--- web/src/layouts/BasicLayout/BasicLayout.js
+++ web/src/layouts/BasicLayout/BasicLayout.js
@@ -14,7 +14,7 @@ const BasicLayout = ({ children }) => {
       <header>
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <svg
+            {/* <svg
               className="fill-current h-8 w-8 mr-2"
               width="54"
               height="54"
@@ -22,7 +22,7 @@ const BasicLayout = ({ children }) => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
+            </svg> */}
             <span className="font-semibold text-xl tracking-tight">
               <Link to={routes.home()}>Mintbean Hackathon</Link>
             </span>
@@ -60,14 +60,6 @@ const BasicLayout = ({ children }) => {
               >
                 About
               </Link>
-            </div>
-            <div>
-              <a
-                href="#"
-                className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-              >
-                Download
-              </a>
             </div>
           </div>
         </nav>
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -7,9 +7,7 @@ const HomePage = () => {
       <div className="container mx-auto">
         <h1 className="text-lg font-bold">Mintbean Hackathon 6-22</h1>
         <p className="text-md font-hairline">Weatherhack</p>
-        <div className="bg-gray-200 p4">
-          <BlogPostsCell />
-        </div>
+        <div className="bg-gray-200 p4"></div>
       </div>
     </BasicLayout>
   )
