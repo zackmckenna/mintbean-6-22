1a2dd890c2d9776d08b4405eb809437f9bc8f37f
Zack McKenna <zackmckenna@gmail.com>
2020-06-24 16:53:24 -0400

    updated info page

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -86,6 +86,25 @@ const AboutPage = () => {
           </thead>
           <tbody>{tableData.map((row) => generateRow(row))}</tbody>
         </table>
+        <div className="bg-orange-300 rounded mt-4 p-2">
+          <h2 className="text-xl font-bold">Air Quality Observations</h2>
+          <p className="leading-relaxed">
+            Hourly or daily observations are measured and reported to AirNow by
+            federal, state, local, and tribal air quality agencies. Historical
+            (daily) AQI values are calculated using an averaging method, and
+            real-time AQI values are based on a surrogate calculation.
+          </p>
+          <h2 className="text-xl mt-3 font-bold">Data averaging</h2>
+          <p className="leading-relaxed">
+            The AQI reported for ground-level ozone and fine particles (PM2.5)
+            is based on an average of hourly data. For ozone, the AQI is based
+            on the maximum observed 8-hour average from midnight to midnight.
+            For PM2.5, the AQI is simply the 24-hour average. For AQI values
+            reported in real-time, before a full day's data are available, the
+            AQI is based on a surrogate calculation.
+          </p>
+        </div>
+        <p className="mt-3 mb-2 text-center">info courtesy airnow.gov</p>
       </div>
     </BasicLayout>
   )
