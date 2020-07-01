20c125235377a9e45e4dada8792dbc7521413655
Zack McKenna <zackmckenna@gmail.com>
2020-07-01 16:46:41 -0400

    added dynamic date to quality service

--- api/src/services/quality/quality.js
+++ api/src/services/quality/quality.js
@@ -1,8 +1,10 @@
 import fetch from 'node-fetch'
 
 export const getQuality = async ({ zip }) => {
+  const date = new Date().toISOString().slice(0, 10)
+  console.log(date)
   const response = await fetch(
-    `http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${zip}&date=2020-06-24&distance=25&API_KEY=5DBA34CF-223A-4431-8A57-8907908C7E43`
+    `http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${zip}&date=${date}&distance=25&API_KEY=5DBA34CF-223A-4431-8A57-8907908C7E43`
   )
   const json = await response.json()
   const descriptor = (aqi) => {
